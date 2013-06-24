6d827b10f3a680e292b89de089b45db36ebe46f5
bitstorm <an.delbene@gmail.com>
2013-06-24 22:12:55 +0200

    Added Apache 2 license

--- WicketAnnotations/src/main/java/com/mycompany/resource/AbstractRestResource.java
+++ WicketAnnotations/src/main/java/com/mycompany/resource/AbstractRestResource.java
@@ -1,3 +1,19 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.mycompany.resource;
 
 import java.io.BufferedReader;
@@ -28,6 +44,7 @@ import org.apache.wicket.request.resource.AbstractResource;
 import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.util.string.StringValue;
 import org.apache.wicket.util.string.StringValueConversionException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import com.mycompany.annotations.HttpMethod;
 import com.mycompany.annotations.JsonBody;
@@ -118,8 +135,8 @@ public class AbstractRestResource implements IResource {
 			CharBuffer target = CharBuffer.allocate(100);
 			
 			bufReader.read(target);
-			
-			return "hallo";
+			ObjectMapper mapper = new ObjectMapper();
+			return mapper.readValue(target.toString(), argClass);			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
